38cf83cf218da3663176274e397057654ba65c32
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 22:22:32 +0900

    Use fixed GitHub Pages redirect URI

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.RECORD_DIVE_CONFIG = {
   CLIENT_ID: "c783186baa364ae28193ee4f0095d5cc",
-  REDIRECT_URI: window.location.origin + window.location.pathname,
+  REDIRECT_URI: "https://kumakitiho.github.io/spotify-richweb-service/",
   SCOPES: "user-top-read user-read-private user-read-email",
   TOKEN_ENDPOINT: "https://accounts.spotify.com/api/token",
   AUTH_URL: "https://accounts.spotify.com/authorize"
