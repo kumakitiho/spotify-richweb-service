013e21a01409082e7d9f9bdda2b7a6abd8eb8503
kumakitiho <s.kuma1015@gmail.com>
2026-07-05 21:12:38 +0900

    Set Spotify client ID

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.RECORD_DIVE_CONFIG = {
-  CLIENT_ID: "YOUR_SPOTIFY_CLIENT_ID_HERE",
+  CLIENT_ID: "c783186baa364ae28193ee4f0095d5cc",
   REDIRECT_URI: window.location.origin + window.location.pathname,
   SCOPES: "user-top-read user-read-private user-read-email",
   TOKEN_ENDPOINT: "https://accounts.spotify.com/api/token",
