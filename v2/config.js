aae23a473692be62d58b9ac0b0bd942199811bf0
kumakitiho <s.kuma1015@gmail.com>
2026-07-07 04:29:50 +0900

    Add v2 Spotify redirect config

---
+++ v2/config.js
@@ -0,0 +1,7 @@
+window.RECORD_DIVE_CONFIG = {
+  CLIENT_ID: "c783186baa364ae28193ee4f0095d5cc",
+  REDIRECT_URI: "https://kumakitiho.github.io/spotify-richweb-service/v2/",
+  SCOPES: "user-top-read user-read-private user-read-email",
+  TOKEN_ENDPOINT: "https://accounts.spotify.com/api/token",
+  AUTH_URL: "https://accounts.spotify.com/authorize"
+};
